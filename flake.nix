11d589876d59dcc068d26bda1bac85c4b1fad8cc
JP <dragonginger10@gmail.com>
2023-09-08 13:08:06 -0500

    git and nix files

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ python3 ] ++
+          (with pkgs.python3Packages; [ pip requests typer ]);
+      };
+    });
+}
