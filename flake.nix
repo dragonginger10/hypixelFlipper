2adb52f27713a11a17b2f651d47cb3e88e3fd563
JP <dragonginger10@gmail.com>
2023-09-11 15:00:41 -0500

    set up flipper package

--- flake.nix
+++ flake.nix
@@ -6,31 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ python3 ] ++
-          (with pkgs.python3Packages; [ pip requests typer python-dotenv rich ]);
+        packages = with pkgs;
+          [python3]
+          ++ (with pkgs.python3Packages; [pip requests typer python-dotenv rich]);
       };
 
-      packages.default = with pkgs.python3Packages;
-        buildPythonApplication {
-          pname = "flipper";
-          version = "0.1";
-          format = "pyproject";
+      packages = rec {
+        default = flipper;
 
-          propagatedBulidInputs = [ setuptools requests typer rich python-dotenv];
+        flipper = with pkgs.python3Packages;
+          buildPythonApplication {
+            pname = "flipper";
+            version = "0.1";
+            format = "pyproject";
 
-          src = ./.;
-        };
+            propagatedBulidInputs = [setuptools requests typer rich python-dotenv];
+
+            src = ./.;
+          };
+      };
     });
 }
