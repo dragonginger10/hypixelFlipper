59943cd6d49a03a330a304ae8ec535063ecf3838
JP <dragonginger10@gmail.com>
2023-09-11 13:38:21 -0500

    print out response and quick status

--- flipper/callApi.py
+++ flipper/callApi.py
@@ -1,10 +1,17 @@
 import requests
+from rich import print
 
 BAZAAR="https://api.hypixel.net/skyblock/bazaar"
 
-response = requests.get(BAZAAR)
+def callapi():
+    response = requests.get(BAZAAR)
 
-if response.status_code != "200":
-    print("API call failed")
-else:
-    print("API call success")
+    if response.status_code != 200:
+        print("API call failed")
+    else:
+        print("API call success")
+
+    r_json = response.json()
+
+    for product in r_json["products"]:
+        print(product["quick_status"])
