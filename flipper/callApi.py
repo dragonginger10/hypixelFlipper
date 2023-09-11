ed53cedd84c300bdd74e04768bf2d9af67e79886
JP <dragonginger10@gmail.com>
2023-09-11 14:27:26 -0500

    create basic table to show possible profitable flips

--- flipper/callApi.py
+++ flipper/callApi.py
@@ -1,5 +1,8 @@
+import sys
+
 import requests
-from rich import print
+from rich.console import Console
+from rich.table import Table
 
 BAZAAR="https://api.hypixel.net/skyblock/bazaar"
 
@@ -8,10 +11,37 @@ def callapi():
 
     if response.status_code != 200:
         print("API call failed")
-    else:
-        print("API call success")
+        sys.exit(1)
 
     r_json = response.json()
 
-    for product in r_json["products"]:
-        print(product["quick_status"])
+    products = r_json["products"]
+
+    table = Table(title="Bazaar Flips")
+
+    table.add_column("Product", justify="center", style="cyan")
+    table.add_column("Sell price", justify="center", style="green")
+    table.add_column("Buy Price", justify="center", style="red")
+    table.add_column("Profit Margin", justify="center", style="green")
+
+    product_list = []
+
+    for i in products:
+        id = products[i]["quick_status"]["productId"]
+        sell_price = products[i]["quick_status"]["sellPrice"]
+        buy_price = products[i]["quick_status"]["buyPrice"]
+
+        if sell_price == 0.0 or buy_price == 0.0:
+            continue
+
+        margin = (buy_price/sell_price)
+
+        if margin > 100:
+            product_list.append((id, sell_price, buy_price, margin))
+
+
+    for id, sell_price, buy_price, margin in sorted(product_list, key=lambda item: item[3], reverse=True):
+        table.add_row(id, '${:,.2f}'.format(sell_price), '${:,.2f}'.format(buy_price), '{:,.1f}%'.format(margin))
+
+    console = Console()
+    console.print(table)
