d6f5d9032df0fe7334070de4e8f22bd36bf249ae
JP <dragonginger10@gmail.com>
2023-09-11 18:29:39 -0500

    create df in callapi module

--- flipper/callApi.py
+++ flipper/callApi.py
@@ -1,8 +1,7 @@
 import sys
+import pandas as pd
 
 import requests
-from rich.console import Console
-from rich.table import Table
 
 BAZAAR="https://api.hypixel.net/skyblock/bazaar"
 
@@ -14,18 +13,8 @@ def callapi():
         sys.exit(1)
 
     r_json = response.json()
-
     products = r_json["products"]
-
-    table = Table(title="Bazaar Flips")
-
-    table.add_column("Product", justify="center", style="cyan")
-    table.add_column("Sell price", justify="center", style="green")
-    table.add_column("Buy Price", justify="center", style="red")
-    table.add_column("Instant Buys", justify="center", style="cyan")
-    table.add_column("Profit Margin", justify="center", style="green")
-
-    product_list = []
+    results = []
 
     for i in products:
         id = products[i]["quick_status"]["productId"]
@@ -33,17 +22,13 @@ def callapi():
         buy_price = products[i]["quick_status"]["buyPrice"]
         buy_week = products[i]["quick_status"]["buyMovingWeek"]
 
-        if sell_price == 0.0 or buy_price == 0.0:
-            continue
+        data = {
+            "id": id,
+            "sell_price": sell_price,
+            "buy_price": buy_price,
+            "buy_week": buy_week
+        }
 
-        margin = (buy_price/sell_price)
+        results.append(data)
 
-        if margin > 100 and buy_week > 1000:
-            product_list.append((id, sell_price, buy_price, margin, buy_week))
-
-
-    for id, sell_price, buy_price, margin, buy_week in sorted(product_list, key=lambda item: item[3], reverse=True):
-        table.add_row(id, '${:,.2f}'.format(sell_price), '${:,.2f}'.format(buy_price), '{:,}'.format(buy_week),'{:,.1f}%'.format(margin))
-
-    console = Console()
-    console.print(table)
+    df = pd.DataFrame(results)
