95ad5fa674e2d6c389d0c703de678461a80c0f91
JP <dragonginger10@gmail.com>
2023-09-11 14:48:41 -0500

    added instabuys in last 7days to show realistic buy rate

--- flipper/callApi.py
+++ flipper/callApi.py
@@ -22,6 +22,7 @@ def callapi():
     table.add_column("Product", justify="center", style="cyan")
     table.add_column("Sell price", justify="center", style="green")
     table.add_column("Buy Price", justify="center", style="red")
+    table.add_column("Instant Buys", justify="center", style="cyan")
     table.add_column("Profit Margin", justify="center", style="green")
 
     product_list = []
@@ -30,18 +31,19 @@ def callapi():
         id = products[i]["quick_status"]["productId"]
         sell_price = products[i]["quick_status"]["sellPrice"]
         buy_price = products[i]["quick_status"]["buyPrice"]
+        buy_week = products[i]["quick_status"]["buyMovingWeek"]
 
         if sell_price == 0.0 or buy_price == 0.0:
             continue
 
         margin = (buy_price/sell_price)
 
-        if margin > 100:
-            product_list.append((id, sell_price, buy_price, margin))
+        if margin > 100 and buy_week > 1000:
+            product_list.append((id, sell_price, buy_price, margin, buy_week))
 
 
-    for id, sell_price, buy_price, margin in sorted(product_list, key=lambda item: item[3], reverse=True):
-        table.add_row(id, '${:,.2f}'.format(sell_price), '${:,.2f}'.format(buy_price), '{:,.1f}%'.format(margin))
+    for id, sell_price, buy_price, margin, buy_week in sorted(product_list, key=lambda item: item[3], reverse=True):
+        table.add_row(id, '${:,.2f}'.format(sell_price), '${:,.2f}'.format(buy_price), '{:,}'.format(buy_week),'{:,.1f}%'.format(margin))
 
     console = Console()
     console.print(table)
